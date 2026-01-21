3a1d68b9c682fecccfca248ce527b884c5f1e961
mootezammar <mootezammar4@gmail.com>
2026-01-21 13:52:43 +0100

    hu

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import { assets } from "../assets/assets";
 
 const Footer = () => {
   return (
-    <footer className="mt-auto mx-auto px-6 md:px-16 lg:px-24 xl:px-32 pt-8 w-full text-gray-500">
+    <footer className="mt-auto mx-auto px-6 md:px-16 lg:px-24 xl:px-32 pt-8 w-full text-gray-500 mt-20">
       <div className="flex flex-col md:flex-row justify-between w-full gap-10 border-b border-gray-500/30 pb-6">
         <div className="md:max-w-96">
           <img className="h-9" src={assets.logo} alt="logo" />
@@ -46,7 +46,7 @@ const Footer = () => {
                   type="email"
                   placeholder="Enter your email"
                 />
-                <button className="bg-blue-600 w-24 h-9 text-white rounded">
+                <button className="bg-primary w-24 h-9 text-white rounded cursor-pointer">
                   Subscribe
                 </button>
               </div>
@@ -55,8 +55,8 @@ const Footer = () => {
         </div>
       </div>
       <p className="pt-4 text-center text-xs md:text-sm pb-5">
-        Copyright 2024 © <a href="https://prebuiltui.com">PrebuiltUI</a>. All
-        Right Reserved.
+        Copyright 2026 © aitools by mootez{" "}
+        <a href="https://prebuiltui.com">PrebuiltUI</a>. All Right Reserved.
       </p>
     </footer>
   );
